3b9c8ae7531d35838a5212cd4591ce5a851a0b30
Ista Zahn <istazahn@gmail.com>
2021-10-07 12:34:03 -0400

    cleanup build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,6 @@
 
 mkdir build
 
-cmake -B build -S . ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX -G Ninja
+cmake -B build -S . ${CMAKE_ARGS} -DISO_CODES_PREFIX=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -G Ninja
 cmake --build build
-DESTDIR=$PREVIX cmake --install build
+cmake --install build
